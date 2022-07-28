0d3f97d5c21ce864cddcdf19b0e509c320c2422a
sam <sam.barrowclough@gmail.com>
2022-07-28 12:59:38 +0100

    /verify {address} done

--- commands/verify.js
+++ commands/verify.js
@@ -109,11 +109,19 @@ module.exports = {
           embeds: [
             new MessageEmbed()
               .setColor(hasEntry ? 'GREEN' : 'RED')
-              .setTitle(hasEntry ? `Visit ${project.name} on Joinlist` : `Visit ${project.name} on Joinlist to register`)
+              .setTitle(
+                hasEntry
+                  ? `Visit ${project.name} on Joinlist`
+                  : `Visit ${project.name} on Joinlist to register`
+              )
               .setURL(`https://joinlist.me/${project.slug}`) // could be the url which would show the address in the Verify input. e.g joinlist.me/{project}?address={address}
               .addFields(
                 { name: '\u200B', value: '\u200B' },
-                ...accounts.map(({ provider, username }) => ({ name: provider, value: username, inline: true })),
+                ...accounts.map(({ provider, username }) => ({
+                  name: provider,
+                  value: username,
+                  inline: true
+                })),
                 { name: '\u200B', value: '\u200B' }
               )
               .setDescription(
@@ -130,69 +138,115 @@ module.exports = {
           ]
         })
       } else {
-        // verify the given address is registered on the project
-        log('[COMMAND][VERIFY] api/v1/discord getting...', guildId)
-        // https://www.joinlist.me/api/v1/discord?serverId=902229215993282581
-        const { data: discordData } = await get(
-          `https://www.joinlist.me/api/v1/discord?serverId=${guildId}`
-        )
-        log('[COMMAND][VERIFY] api/v1/discord response', discordData)
+        console.log('discordUserId', discordUserId, 'guildId', guildId)
 
-        if (discordData?.length === 0) {
-          // TODO: better error message
-          return interaction.reply({
-            content: 'No Joinlist project found for this guild.',
-            ephemeral: true
-          })
+        // get userId from account where providerAccountId = address
+        const userId = (
+          await db
+            .from('Account')
+            .select('userId')
+            .match({ providerAccountId: address })
+            ?.single()
+        )?.data?.userId
+        if (!userId) {
+          throw Error(`No user found for address ${address}`)
         }
 
-        const projectId = discordData[0].projectId
-
-        // now get the project details givent the projectId
-        log('[COMMAND][VERIFY] api/v4/projects getting...', projectId)
-        const { data: projectData } = await get(
-          `https://www.joinlist.me/api/v4/projects/${projectId}`
+        // get all accounts for userId
+        // get the user accounts given the userId
+        const profiles = (
+          await db
+            .from('Account')
+            .select('provider, providerAccountId, profile')
+            .match({ userId })
+        )?.data
+        const accounts = profiles.map(
+          ({ provider, providerAccountId, profile }) => {
+            if (provider === 'siwe') {
+              return {
+                provider: 'ethereum',
+                username: profile?.preferred_username ?? providerAccountId
+              }
+            } else if (provider === 'twitter') {
+              return {
+                provider,
+                username: profile?.screen_name
+              }
+            } else if (provider === 'discord') {
+              return {
+                provider,
+                username: profile?.username
+              }
+            }
+          }
         )
-        log('[COMMAND][VERIFY] api/v4/projects response', projectData)
 
-        if (!projectData) {
-          return interaction.reply({
-            content: 'No project found for this guild.',
-            ephemeral: true
-          })
+        // get the projectId  from Discord where serverId = guildId
+        const projectId = (
+          await db
+            .from('Discord')
+            .select('projectId')
+            .match({ serverId: guildId })
+            .single()
+        )?.data?.projectId
+
+        if (!projectId) {
+          throw Error('Project not found')
         }
 
-        const { slug, name, id } = projectData
+        // check if the user has an entry for this project
+        const hasEntry =
+          (
+            await db
+              .from('Entry')
+              .select('id')
+              .match({ userId, projectId })
+              .single()
+          )?.data?.id != null
 
-        // now get the project details givent the projectId
-        log('[COMMAND][VERIFY] api/v1/entries getting...', projectId)
-        const { data } = await get(
-          `https://www.joinlist.me/api/v1/entries?address=${address}&projectId=${id}`
-        )
-        log('[COMMAND][VERIFY] api/v1/entries response', data)
+        if (!hasEntry) {
+          throw Error('User does not have an entry for this project')
+        }
 
-        const isVerified = data?.length > 0
-
-        log('[COMMAND][VERIFY] isVerified', isVerified)
+        // get the project from the projectId
+        const project = (
+          await db
+            .from('Project')
+            .select('id, name, slug')
+            .match({ id: projectId })
+            .single()
+        )?.data
 
         interaction.reply({
           embeds: [
             new MessageEmbed()
-              .setColor(isVerified ? 'GREEN' : 'RED')
-              .setTitle(name)
-              .setURL(`https://joinlist.me/${slug}`) // could be the url which would show the address in the Verify input. e.g joinlist.me/{project}?address={address}
+              .setColor(hasEntry ? 'GREEN' : 'RED')
+              .setTitle(
+                hasEntry
+                  ? `Visit ${project.name} on Joinlist`
+                  : `Visit ${project.name} on Joinlist to register`
+              )
+              .setURL(`https://joinlist.me/${project.slug}`) // could be the url which would show the address in the Verify input. e.g joinlist.me/{project}?address={address}
+              .addFields(
+                { name: '\u200B', value: '\u200B' },
+                ...accounts.map(({ provider, username }) => ({
+                  name: provider,
+                  value: username,
+                  inline: true
+                })),
+                { name: '\u200B', value: '\u200B' }
+              )
               .setDescription(
-                isVerified
-                  ? 'Wallet successfully registered'
-                  : 'Wallet not registered'
+                hasEntry
+                  ? '✅ Wallet successfully registered'
+                  : '❌ Wallet not registered'
               )
               .setThumbnail(
-                isVerified
+                hasEntry
                   ? 'https://dpspszizureppmrxkcri.supabase.co/storage/v1/object/public/files/tick.png'
                   : 'https://dpspszizureppmrxkcri.supabase.co/storage/v1/object/public/files/cross.png'
               )
               .setTimestamp()
-              .setFooter(`Address: ${address}`)
           ]
         })
       }
