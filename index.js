3d3a80ec200a323a90497c60980faff794db9278
sam <sam.barrowclough@gmail.com>
2023-01-12 17:57:57 +0000

    Thu 12 Jan 2023 17:57:57 GMT

--- index.js
+++ index.js
@@ -69,178 +69,172 @@ client.once("ready", async () => {
    * Announce the raffle
    */
   (async () => {
-    try {
-      const channel = client.channels.cache.get("1002460057222520955");
-      const project = await getProject({
-        projectId: 1474,
-        select: "*",
-      });
-      const exampleEmbed = new EmbedBuilder()
-        .setColor(0x0099ff)
-        .setTitle(project.name)
-        .setURL("https://discord.js.org/")
-        // .setAuthor({
-        //   name: "Some name",
-        //   iconURL: "https://i.imgur.com/AfFp7pu.png",
-        //   url: "https://discord.js.org",
-        // })
-        .setDescription(project?.description)
-        .setThumbnail(project.image)
-        .addFields({ name: "\u200B", value: "\u200B" })
-        .addFields({
-          name: "Ends at",
-          // project.endAt format is ISO 2023-01-12 12:37:54.53+00, format to Thu Jan 15:15 format
-          value: new Date(project.endAt).toLocaleString(),
-        })
-        //.addFields({ name: "\u200B", value: "\u200B" })
-        // .addFields({
-        //   name: "Inline field title",
-        //   value: "Some value here",
-        // })
-        // //.addFields({ name: "\u200B", value: "\u200B" })
-        // .addFields({
-        //   name: "Inline field title",
-        //   value: "Some value here!",
-        // })
-        .addFields({ name: "\u200B", value: "\u200B" })
-        // .setImage(
-        //   "https://pbs.twimg.com/profile_banners/1487866247803027457/1643571790"
-        // )
-        .setTimestamp();
-      // .setFooter({
-      //   text: "Some footer text here",
-      //   iconURL: "https://i.imgur.com/AfFp7pu.png",
-      // });
+    db.from("Test")
+      .on("*", async (payload) => {
+        try {
+          // const projectId = 1474;
+          // const channelId = "1002460057222520955";
+          // const type = "raffle";
 
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-          //.setCustomId("primary")
-          .setLabel("Go to project")
-          .setStyle(ButtonStyle.Link)
-          .setURL(`https://www.joinlist.me/${project.slug}`)
-      );
+          console.log("payload", payload);
 
-      channel.send({ embeds: [exampleEmbed], components: [row] });
-    } catch (error) {
-      if (error) console.error(error);
-    }
+          const { channelId, projectId, type } = payload.new;
+
+          const channel = client.channels.cache.get(channelId);
+
+          if (!channel) {
+            console.error("Channel not found");
+            return;
+          }
+
+          if (type !== "raffle") {
+            console.error("Type is not raffle");
+            return;
+          }
+
+          const project = await getProject({
+            projectId,
+            select: "*",
+          });
+
+          /**
+           * Get the author of the project
+           */
+          const userId = project?.userId;
+          const twitterProfile = (
+            await db
+              .from("Account")
+              .select("*")
+              .match({ userId, provider: "twitter" })
+              .single()
+          )?.data?.profile;
+
+          const { profile_image_url_https, name } = twitterProfile || {};
+
+          console.log("twitterProfile", twitterProfile);
+
+          const exampleEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle(project.name)
+            .setURL(`https://www.joinlist.me/${project.slug}`)
+            .setAuthor({
+              name,
+              iconURL: profile_image_url_https,
+              //url: "https://discord.js.org",
+            })
+            .setDescription(project?.description)
+            .setThumbnail(project.image)
+            .addFields({ name: "\u200B", value: "\u200B" })
+            .addFields({
+              name: "Ends at",
+              // project.endAt format is ISO 2023-01-12 12:37:54.53+00, format to Thu Jan 15:15 format
+              value: new Date(project.endAt).toLocaleString(),
+            })
+            //.addFields({ name: "\u200B", value: "\u200B" })
+            // .addFields({
+            //   name: "Inline field title",
+            //   value: "Some value here",
+            // })
+            // //.addFields({ name: "\u200B", value: "\u200B" })
+            // .addFields({
+            //   name: "Inline field title",
+            //   value: "Some value here!",
+            // })
+            .addFields({ name: "\u200B", value: "\u200B" })
+            //.setImage(project.bannerImage)
+            .setTimestamp();
+          // .setFooter({
+          //   text: "Some footer text here",
+          //   iconURL: "https://i.imgur.com/AfFp7pu.png",
+          // });
+
+          const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+              //.setCustomId("primary")
+              .setLabel("Go to project")
+              .setStyle(ButtonStyle.Link)
+              .setURL(`https://www.joinlist.me/${project.slug}`)
+          );
+
+          channel.send({ embeds: [exampleEmbed], components: [row] });
+        } catch (error) {
+          if (error) console.error(error);
+        }
+      })
+      .subscribe();
   })();
 
   /**
    * Announce winners of the raffle
    */
 
-  (async () => {
-    db.from("Project")
-      .on("UPDATE", async (payload) => {
-        // Payload looks like:
-        // {
-        //   schema: 'public',
-        //   table: 'Project',
-        //   commit_timestamp: '2023-01-12T14:10:37Z',
-        //   eventType: 'UPDATE',
-        //   new: {
-        //     id: 1474,
-        //     html: null,
-        //     ogImage: null,
-        //     mintPrice: null,
-        //     status: 'sfd',
-        //     type: null,
-        //     questionRequired: null,
-        //     raffleQuantity: null,
-        //     startAt: null,
-        //     showEntries: true,
-        //     image: null,
-        //     supply: null,
-        //     addressRequired: null,
-        //     phoneRequired: null,
-        //     updatedAt: '2023-01-12T12:37:54.53+00:00',
-        //     questionText: null,
-        //     verified: false,
-        //     website: null,
-        //     contractsMustOwn: 'any',
-        //     userId: 9543,
-        //     hasBrandingEnabled: true,
-        //     waitlistSearch: false,
-        //     mintSupply: null,
-        //     createdAt: '2023-01-12T11:54:04.983+00:00',
-        //     maxEntries: null,
-        //     questionType: null,
-        //     twoFactorAuth: null,
-        //     public: true,
-        //     endAt: null,
-        //     reward: false,
-        //     description: null,
-        //     themeCustom: null,
-        //     mintDate: null,
-        //     slug: 'tkja0',
-        //     emailRequired: null,
-        //     name: 'Doodles X Copy',
-        //     nameRequired: null,
-        //     chain: null
-        //   },
-        //   old: { id: 1474 },
-        //   errors: null
-        // }
+  // (async () => {
+  //   db.from("Announcement")
+  //     .on("INSERT", async (payload) => {
+  //       // Payload looks like:
+  //       // {
+  //       //   schema: 'public',
+  //       //   table: 'Announcement',
+  //       //   commit_timestamp: '2023-01-12T15:49:32Z',
+  //       //   eventType: 'INSERT',
+  //       //   new: { channelId: '1002460057222520955', id: 2, projectId: '1474' },
+  //       //   old: {},
+  //       //   errors: null
+  //       // }
 
-        console.log("Got payload: ", payload);
+  //       console.log("Got payload: ", payload);
 
-        try {
-          const projectId = payload.new.id;
-          const status = payload.new.status;
-          const projectName = payload.new.name;
+  //       try {
+  //         const projectId = payload.new.projectId;
+  //         const type = payload.new.stype;
+  //         const channelId = payload.new.channelId;
 
-          if (status !== "drawn") {
-            return console.log("Project is not drawn yet");
-          }
+  //         if (type !== "winner") {
+  //           return console.log("Project is not drawn yet");
+  //         }
 
-          // Get the channelId from the database from the projectId
-          const { raffleChannelId } =
-            (
-              await db
-                .from("Discord")
-                .select("raffleChannelId, projectName")
-                .match({ projectId })
-                .single()
-            )?.data || {};
+  //         /**
+  //          * Get project name
+  //          */
+  //         const projectName = (await getProject({ projectId }))?.name;
 
-          if (!raffleChannelId) {
-            return console.log(
-              `No channel id found for projectId ${projectId}!`
-            );
-          }
+  //         if (!channelId) {
+  //           return console.log(
+  //             `No channel id found for projectId ${projectId}!`
+  //           );
+  //         }
 
-          console.log(
-            `Got channelId: ${raffleChannelId} from project: ${projectName}`
-          );
+  //         console.log(
+  //           `Got channelId: ${channelId} from project: ${projectName}`
+  //         );
 
-          // Get the list of winners from Entry Table given the projectId. filter the winners and only get discordUserId
-          const entries = await getWinners(projectId);
+  //         // Get the list of winners from Entry Table given the projectId. filter the winners and only get discordUserId
+  //         const entries = await getWinners(projectId);
 
-          if (entries?.length === 0) {
-            return console.log("No winners found");
-          }
+  //         if (entries?.length === 0) {
+  //           return console.log("No winners found");
+  //         }
 
-          console.log(
-            `Got entries from project: ${projectName} : ${entries?.length}`
-          );
+  //         console.log(
+  //           `Got entries from project: ${projectName} : ${entries?.length}`
+  //         );
 
-          // Build the message using the list of winners and pass it in the channel
-          const builtMessage = entries?.map((x) => `<@${x}>`).join(", ");
+  //         // Build the message using the list of winners and pass it in the channel
+  //         const builtMessage = entries?.map((x) => `<@${x}>`).join(", ");
 
-          // Send the message
-          const channel = client.channels.cache.get(raffleChannelId);
-          channel.send(`Winners for ${projectName}: ${builtMessage}`);
+  //         // Send the message
+  //         const channel = client.channels.cache.get(channelId);
+  //         channel.send(`Winners for ${projectName}: ${builtMessage}`);
 
-          console.log(`Sent winners for ${projectName} : ${entries.length}`);
-        } catch (error) {
-          console.log(error);
+  //         console.log(`Sent winners for ${projectName} : ${entries.length}`);
+  //       } catch (error) {
+  //         console.log(error);
 
-          // TODO: ping my telegram bot
-        }
-      })
-      .subscribe();
-  })();
+  //         // TODO: ping my telegram bot
+  //       }
+  //     })
+  //     .subscribe();
+  // })();
 
   /**
    * Registering the commands
--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,24 @@
-const { createClient } = require('@supabase/supabase-js')
-require('dotenv').config()
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 const supabaseUrl = `https://${
   process.env.SUPABASE_PROJECT_ID || process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID
-}.supabase.co`
+}.supabase.co`;
 const supabaseKey =
-  process.env.SUPABASE_KEY || process.env.NEXT_PUBLIC_SUPABASE_KEY
-const db = createClient(supabaseUrl, supabaseKey)
+  process.env.SUPABASE_KEY || process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+const options = {
+  schema: "public",
+  headers: { "x-my-custom-header": "joinlist-bot" },
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: true,
+};
+const db = createClient(
+  supabaseUrl,
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwc3Bzeml6dXJlcHBtcnhrY3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1NzQxNjMsImV4cCI6MTk2OTE1MDE2M30._ux0yI5Kct8aTQS_KhCRDSqMgRtxmY_skFDFOMOlI8Q",
+  options
+);
 
 module.exports = {
-  db
-}
+  db,
+};
